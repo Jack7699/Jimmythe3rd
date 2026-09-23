750b62144d1622e8a31a2345d43c6f3fb83ff149
Jack7699 <hvth53873@gmail.com>
2026-09-23 18:46:21 +0100

    Update guessarmy.js

--- src/interactions/buttons/guessarmy/guessarmy.js
+++ src/interactions/buttons/guessarmy/guessarmy.js
@@ -1,1 +1,11 @@
+import {
+  guessArmyClueButton,
+  guessArmyGuessButton,
+  guessArmyLeaderboardButton,
+} from '../../../handlers/guessarmy/guessArmyButtons.js';
 
+export default [
+  guessArmyClueButton,
+  guessArmyGuessButton,
+  guessArmyLeaderboardButton,
+];
